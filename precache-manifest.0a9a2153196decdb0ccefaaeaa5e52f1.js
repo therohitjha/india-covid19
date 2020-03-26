f5e094b88e08094148115c68912638688d79e2d0
Rohit <therohitjha@gmail.com>
2020-03-26 17:09:20 +0530

    Updates

---
+++ precache-manifest.0a9a2153196decdb0ccefaaeaa5e52f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed82b81de02a5bf4bc3ca1f344ed85a",
+    "url": "/india-covid19/index.html"
+  },
+  {
+    "revision": "9ba81aa39f48e5dd0aae",
+    "url": "/india-covid19/static/css/main.924ff263.chunk.css"
+  },
+  {
+    "revision": "45677cebaaef7660f627",
+    "url": "/india-covid19/static/js/2.6ecb0c72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/india-covid19/static/js/2.6ecb0c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba81aa39f48e5dd0aae",
+    "url": "/india-covid19/static/js/main.3d4a84b4.chunk.js"
+  },
+  {
+    "revision": "ed84d666d740466867fd",
+    "url": "/india-covid19/static/js/runtime-main.259f6442.js"
+  }
+]);
